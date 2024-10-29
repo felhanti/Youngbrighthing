92a9ab377e5aa98d29441ea51ea99cc1820c5721
Fouad El hanti <fouad.elhanti@gmail.com>
2024-10-29 16:32:00 +0100

    maj des icones, ajout pointe de gold et creation CGV

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -15,4 +15,31 @@ class HomepageController extends AbstractController
             'controller_name' => 'HomepageController',
         ]);
     }
+
+    #[Route('/cgv/cgu', name: 'app_cgv_cgu')]
+    public function cgu(): Response
+    {
+        return $this->render('cgv/cgu.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+
+    }
+
+    #[Route('/cgv/confidentialite', name: 'app_cgv_confidentialite')]
+    public function confidentialite(): Response
+    {
+        return $this->render('cgv/confidentialite.html.twig');
+    }
+
+    #[Route('/cgv/mentions-legales', name: 'app_cgv_mentions_legales')]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('cgv/mentions_legales.html.twig');
+    }
+
+    #[Route('/cgv/retour', name: 'app_cgv_retour')]
+    public function retour(): Response
+    {
+        return $this->render('cgv/retour.html.twig');
+    }
 }
