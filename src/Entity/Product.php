170fc0bf064e1aaed4aee0360affab2cde5d029a
Fouad El hanti <fouad.elhanti@gmail.com>
2024-10-29 10:56:42 +0100

    Ajout du dashboard et ajustement style de la partie product

---
+++ src/Entity/Product.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Description = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column]
+    private ?bool $is_sold = false;
+
+    #[ORM\Column(length: 10)]
+    private ?string $size = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $add_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isSold(): ?bool
+    {
+        return $this->is_sold;
+    }
+
+    public function setSold(bool $is_sold): self
+    {
+        $this->is_sold = $is_sold;
+
+        return $this;
+    }
+
+    public function getSize(): ?string
+    {
+        return $this->size;
+    }
+
+    public function setSize(string $size): static
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function __construct()
+    {
+        $this->add_date = new \DateTime(); // Définit la date actuelle par défaut
+    }
+    public function getAddDate(): ?\DateTimeInterface
+    {
+        return $this->add_date;
+    }
+
+    public function setAddDate(\DateTimeInterface $add_date): static
+    {
+        $this->add_date = $add_date;
+
+        return $this;
+    }
+}
