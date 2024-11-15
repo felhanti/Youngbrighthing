61ec035fbfde8e690356b0d4deb6b4d54fa2b942
Fouad El hanti <fouad.elhanti@gmail.com>
2024-11-15 11:04:44 +0100

    panier fonctionnel

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,100 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Cart;
 use App\Entity\User;
-use App\Form\CartType;
-use App\Repository\CartRepository;
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/cart')]
 final class CartController extends AbstractController
 {
-    #[Route(name: 'app_cart_index', methods: ['GET'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function index(CartRepository $cartRepository): Response
-    {
-        return $this->render('cart/index.html.twig', [
-            'carts' => $cartRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_cart_new', methods: ['GET', 'POST'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $cart = new Cart();
-        $form = $this->createForm(CartType::class, $cart);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($cart);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('cart/new.html.twig', [
-            'cart' => $cart,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/cart/{id}', name: 'app_cart_show', methods: ['GET'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function showCart(int $id, EntityManagerInterface $entityManager): Response
-    {
-        $cart = $entityManager->getRepository(Cart::class)->find($id);
-        if (!$cart) {
-            throw $this->createNotFoundException("Panier non trouvé.");
-        }
-
-        $connectedUser = $this->getUser();
-        if ($cart->getUser() !== $connectedUser) {
-            throw $this->createAccessDeniedException("Vous n'avez pas accès à ce panier.");
-        }
-
-        return $this->render('cart/show.html.twig', [
-            'cart' => $cart,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_cart_edit', methods: ['GET', 'POST'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function edit(Request $request, Cart $cart, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CartType::class, $cart);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('cart/edit.html.twig', [
-            'cart' => $cart,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_cart_delete', methods: ['POST'])]
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function delete(Request $request, Cart $cart, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $cart->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($cart);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_cart_index', [], Response::HTTP_SEE_OTHER);
-    }
     #[Route('/cart/add/{id}', name: 'app_cart_add', methods: ['POST'])]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function addToCart(int $id, EntityManagerInterface $entityManager, Request $request, ProductRepository $productRepository, User $user): Response
+    public function addToCart(int $id, EntityManagerInterface $entityManager, ProductRepository $productRepository): Response
     {
         // Récupérer le produit par son ID
         $product = $productRepository->find($id);
@@ -120,15 +40,76 @@ final class CartController extends AbstractController
         // Ajouter le produit au panier
         $cart->addProduct($product);
 
-        $cart->setDate(new \DateTime());
+        // Marquer le produit comme vendu
+        $product->setIssold(false);
 
-        // Sauvegarder le panier
+        // Sauvegarder le panier et mettre à jour l'état du produit
         $entityManager->persist($cart);
         $entityManager->flush();
 
-        // Rediriger vers la page du panier ou afficher un message de succès
+        $this->addFlash('success', 'Produit ajouté au panier avec succès.');
+
+        // Rediriger vers la page du panier
         return $this->redirectToRoute('app_cart_show', [
-            'id' => $cart->getId()
+            'id' => $cart->getId(),
         ]);
     }
+
+    #[Route('/{id}', name: 'app_cart_show', methods: ['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function showCart(int $id, EntityManagerInterface $entityManager): Response
+    {
+        // Récupérer le panier par son ID
+        $cart = $entityManager->getRepository(Cart::class)->find($id);
+        if (!$cart) {
+            throw $this->createNotFoundException("Panier non trouvé.");
+        }
+
+        // Vérifier que le panier appartient à l'utilisateur connecté
+        /** @var User $connectedUser */
+        $connectedUser = $this->getUser();
+        if ($cart->getUser() !== $connectedUser) {
+            throw $this->createAccessDeniedException("Vous n'avez pas accès à ce panier.");
+        }
+
+        // Rendre la vue avec le panier
+        return $this->render('cart/show.html.twig', [
+            'cart' => $cart,
+        ]);
+    }
+
+    #[Route('/cart/remove/{id}', name: 'app_cart_remove', methods: ['POST'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function removeFromCart(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $connectedUser = $this->getUser();
+        $cart = $connectedUser->getCarts()->first();
+
+        if (!$cart) {
+            throw $this->createNotFoundException("Panier non trouvé.");
+        }
+
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Produit non trouvé.");
+        }
+
+        // Retirer le produit du panier
+        $cart->removeProduct($product);
+
+        // Marquer le produit comme non vendu
+        $product->setIssold(true);
+
+        // Sauvegarder les changements
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Produit retiré du panier avec succès.');
+
+        // Rediriger vers la page du panier
+        return $this->redirectToRoute('app_cart_show', [
+            'id' => $cart->getId(),
+        ]);
+    }
+
 }
